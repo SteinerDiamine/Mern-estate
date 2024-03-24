f436efd24b73c271bfc679918d202be39c9e042d
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-03-25 03:43:37 +0530

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <h1 className='font-bold text-sm sm:text-xl flex'>
+        <span className='text-slate-500'>Real</span>
+        <span className='text-slate-700'>Real</span>
+      </h1>
+    </header>
+  )
+}
